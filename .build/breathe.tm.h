70b907657e4d0649a693d4c3e623d8b9759c436c
Andrew Dong <andrewdong1994@gmail.com>
2025-03-28 22:31:31 -0400

    Remove .build directory from version control

--- .build/breathe.tm.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#line 1 "/Users/andrewdong/Documents/2025/Recurse/bruce_stuff/breathe/breathe.tm"
-#include <tomo/tomo.h>
-void _$breathe$sleep_with_bar(Text_t _$phase, Int_t _$duration);
-void _$breathe$main(Int_t _$inhale_time, Int_t _$hold_time, Int_t _$exhale_time, Int_t _$cycles);
-void _$breathe$$initialize(void);
--- .build/greet.tm.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#line 1 "/Users/andrewdong/Documents/2025/Recurse/bruce_stuff/breathe/greet.tm"
-#include <tomo/tomo.h>
-void _$greet$main();
-void _$greet$$initialize(void);
